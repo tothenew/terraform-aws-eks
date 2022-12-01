9cac80418d0d953158e6dcb1523f97adf31fde2e
ayushme001 <ayush.me001@gmail.com>
2022-12-01 17:14:57 +0530

    deleted versions

--- modules/terraform-aws-eks-node-termination-handler/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 0.13"
-
-  required_providers {
-    aws        = ">= 3.13, < 4.0"
-    helm       = ">= 1.0, < 3.0"
-    kubernetes = ">= 1.10.0, < 3.0.0"
-  }
-}
