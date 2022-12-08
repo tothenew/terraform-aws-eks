b4b9257190606fb217473e917734f17fc52036b0
ayushme001 <ayush.me001@gmail.com>
2022-12-08 21:54:33 +0530

    added toleration for autoscaler and termination handler

--- modules/terraform-aws-eks-cluster-autoscaler/versions.tf
+++ modules/terraform-aws-eks-cluster-autoscaler/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.74.2"
+      version = ">= 3.72"
     }
     helm       = ">= 1.0, < 3.0"
     kubernetes = {
