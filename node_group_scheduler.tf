cd6373519e28612a3838ca1a86eec82a8a9d3686
Chhavi Sharma <chhavi.sharma@tothenew.com>
2023-02-16 15:42:38 +0530

    removed count var

--- node_group_scheduler.tf
+++ node_group_scheduler.tf
@@ -2,7 +2,7 @@ resource "aws_autoscaling_schedule" "eks_stop" {
   depends_on = [
     module.self_managed_node_group
   ]
-  for_each = var.enable_schedule ? toset(compact([for group in module.self_managed_node_group : group.autoscaling_group_name])) : []
+  for_each = toset([ for group in module.self_managed_node_group : group.autoscaling_group_name if var.enable_schedule])
   scheduled_action_name  = "self-managed-nodegroup-${var.cluster_name}-stop"
   min_size               = 0
   max_size               = 0
@@ -15,7 +15,7 @@ resource "aws_autoscaling_schedule" "eks_start" {
   depends_on = [
     module.self_managed_node_group
   ]
-  for_each = var.enable_schedule ? toset(compact([for group in module.self_managed_node_group : group.autoscaling_group_name])) : []
+  for_each = toset([ for group in module.self_managed_node_group : group.autoscaling_group_name if var.enable_schedule])
   scheduled_action_name  = "self-managed-nodegroup-${var.cluster_name}-start"
   min_size               = try(var.self_managed_node_group_defaults.min_size, 0)
   max_size               = try(var.self_managed_node_group_defaults.max_size, 3)
