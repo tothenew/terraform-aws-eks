971308897ba661de23bdfc63ff411622964750d6
Chhavi Sharma <chhavi.sharma@tothenew.com>
2023-02-16 15:34:48 +0530

    removed count var

--- node_group_scheduler.tf
+++ node_group_scheduler.tf
@@ -2,12 +2,12 @@ resource "aws_autoscaling_schedule" "eks_stop" {
   depends_on = [
     module.self_managed_node_group
   ]
-  count = var.enable_schedule ? length(compact([for group in module.self_managed_node_group : group.autoscaling_group_name])): 0
+  for_each = var.enable_schedule ? toset(compact([for group in module.self_managed_node_group : group.autoscaling_group_name])) : []
   scheduled_action_name  = "self-managed-nodegroup-${var.cluster_name}-stop"
   min_size               = 0
   max_size               = 0
   desired_capacity       = 0
-  autoscaling_group_name = compact([for group in module.self_managed_node_group : group.autoscaling_group_name])[count.index]
+  autoscaling_group_name = each.value
   recurrence             = var.schedule_cron_stop
 }
 
@@ -15,11 +15,11 @@ resource "aws_autoscaling_schedule" "eks_start" {
   depends_on = [
     module.self_managed_node_group
   ]
-  count = var.enable_schedule ? length(compact([for group in module.self_managed_node_group : group.autoscaling_group_name])): 0
+  for_each = var.enable_schedule ? toset(compact([for group in module.self_managed_node_group : group.autoscaling_group_name])) : []
   scheduled_action_name  = "self-managed-nodegroup-${var.cluster_name}-start"
   min_size               = try(var.self_managed_node_group_defaults.min_size, 0)
   max_size               = try(var.self_managed_node_group_defaults.max_size, 3)
   desired_capacity       = try(var.self_managed_node_group_defaults.desired_size, 1)
-  autoscaling_group_name = compact([for group in module.self_managed_node_group : group.autoscaling_group_name])[count.index]
+  autoscaling_group_name = each.value
   recurrence             = var.schedule_cron_start
 }
