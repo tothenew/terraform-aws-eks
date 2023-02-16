3e6e6d86a28682d23b905a173d7f236293419982
Chhavi Sharma <chhavi.sharma@tothenew.com>
2023-02-16 13:17:57 +0530

    modified min max for asg

--- node_group_scheduler.tf
+++ node_group_scheduler.tf
@@ -11,9 +11,9 @@ resource "aws_autoscaling_schedule" "ecs_stop" {
 resource "aws_autoscaling_schedule" "ecs_start" {
   for_each = var.enable_schedule ? toset(compact([for group in module.self_managed_node_group : group.autoscaling_group_name])) : []
   scheduled_action_name  = "self-managed-nodegroup-${var.cluster_name}-start"
-  min_size               = try(each.value.min_size, var.self_managed_node_group_defaults.min_size, 0)
-  max_size               = try(each.value.max_size, var.self_managed_node_group_defaults.max_size, 3)
-  desired_capacity       = try(each.value.desired_size, var.self_managed_node_group_defaults.desired_size, 1)
+  min_size               = try(var.self_managed_node_group_defaults.min_size, 0)
+  max_size               = try(var.self_managed_node_group_defaults.max_size, 3)
+  desired_capacity       = try(var.self_managed_node_group_defaults.desired_size, 1)
   autoscaling_group_name = each.value
   recurrence             = var.schedule_cron_start
 }
